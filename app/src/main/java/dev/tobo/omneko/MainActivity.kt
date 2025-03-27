56632fcaa88454a68c616884be42453ac5e9a8e1
Tobias Wienkoop <git@tobo.dev>
2025-03-27 17:36:02 +0100

    Add ExoPlayer and load the downloaded video

--- app/src/main/java/dev/tobo/omneko/MainActivity.kt
+++ app/src/main/java/dev/tobo/omneko/MainActivity.kt
@@ -1,29 +1,45 @@
 package dev.tobo.omneko
 
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.net.toUri
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.compose.PlayerSurface
 import com.yausername.youtubedl_android.YoutubeDL
 import com.yausername.youtubedl_android.YoutubeDLRequest
 import dev.tobo.omneko.ui.theme.OmnekoTheme
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
+    var player: Player? = null
+    var videoFile: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        videoFile = File("$cacheDir/video")
+        videoFile?.delete()
+
         YoutubeDL.getInstance().init(this)
         val request = YoutubeDLRequest("<video-link>")
-        request.addOption("-o", "$cacheDir/video")
+        request.addOption("-o", videoFile?.path ?: "")
         YoutubeDL.getInstance().execute(request) { progress, etaInSeconds, _ ->
             println("$progress % (ETA $etaInSeconds)")
         }
@@ -32,8 +48,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             OmnekoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    VideoPlayer(
+                        player = player,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -41,24 +57,41 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        player = ExoPlayer.Builder(this).build()
+        player?.setMediaItem(MediaItem.fromUri(videoFile?.toUri() ?: Uri.EMPTY))
+        player?.repeatMode = Player.REPEAT_MODE_ALL
+        player?.prepare()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        player?.playWhenReady = false
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        File(cacheDir, "video").delete()
+        player?.release()
+        videoFile?.delete()
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun VideoPlayer(player: Player? = null, modifier: Modifier = Modifier) {
+    PlayerSurface(
+        player ?: ExoPlayer.Builder(LocalContext.current).build(),
+        modifier.clickable {
+            player?.playWhenReady = !player.playWhenReady
+        }
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PlayerPreview() {
     OmnekoTheme {
-        Greeting("Android")
+        VideoPlayer()
     }
 }
