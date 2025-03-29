c64462cfdd329c1122ae248ff0f7eea862256c9e
Tobias Wienkoop <git@tobo.dev>
2025-03-29 12:32:35 +0100

    Fix compose preview error

--- app/src/main/java/dev/tobo/omneko/MainActivity.kt
+++ app/src/main/java/dev/tobo/omneko/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
@@ -73,12 +74,16 @@ fun VideoPlayer(modifier: Modifier = Modifier, videoUri: Uri?, videoFile: File?,
         viewModel.downloadAndPlayVideo(context, videoUri, videoFile, player)
     }
 
-    PlayerSurface(
-        player ?: ExoPlayer.Builder(context).build(),
-        modifier.clickable {
-            player?.playWhenReady = !player.playWhenReady
-        }
-    )
+    if (player != null) {
+        PlayerSurface(
+            player,
+            modifier.clickable {
+                player.playWhenReady = !player.playWhenReady
+            }
+        )
+    } else {
+        Surface(modifier.fillMaxSize()) {  }
+    }
 }
 
 @Preview(showBackground = true)
