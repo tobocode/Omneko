629a55b64af87ce4c39fa0cf529c8acf4af89094
Tobias Wienkoop <git@tobo.dev>
2025-04-06 17:50:27 +0200

    Add new MainActivity

---
+++ app/src/main/java/dev/tobo/omneko/MainActivity.kt
@@ -0,0 +1,79 @@
+package dev.tobo.omneko
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.tobo.omneko.ui.theme.OmnekoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MainLayout()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainLayout(modifier: Modifier = Modifier) {
+    OmnekoTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                LargeTopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                        titleContentColor = MaterialTheme.colorScheme.inverseSurface
+                    ),
+                    title = {
+                        Text(LocalContext.current.getString(R.string.app_name))
+                    }
+                )
+            }
+        ) { innerPadding ->
+            Column(
+                modifier = modifier.padding(innerPadding).padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                Text("If you have trouble watching videos, try updating YoutubeDL")
+
+                Button(modifier = Modifier.fillMaxWidth(),
+                    onClick = {}) {
+                    Text("Update YoutubeDL")
+                }
+
+                LinearProgressIndicator(
+                    progress = { 0.66f },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MainLayout()
+}
