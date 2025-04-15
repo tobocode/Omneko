13a9f71b68557ff7cfe3acd0e3229f4fc60e46e9
Tobias Wienkoop <git@tobo.dev>
2025-04-15 12:53:53 +0200

    Bump version to 1.0.0-alpha.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.tobo.omneko"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.0.0-alpha.9"
+        versionCode = 10
+        versionName = "1.0.0-alpha.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
