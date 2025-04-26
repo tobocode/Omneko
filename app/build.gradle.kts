2d12e6a1633c7b8d6b5d53e89677f2560d18b78b
Tobias Wienkoop <git@tobo.dev>
2025-04-26 13:07:51 +0200

    Bump version to 1.0.0-rc.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.tobo.omneko"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.0.0-rc.2"
+        versionCode = 14
+        versionName = "1.0.0-rc.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
